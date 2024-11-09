88b13051767c00e9bea05f90cdcf19a439cd58d8
Enoz <adudek013@gmail.com>
2024-11-09 13:26:01 -0600

    Working RPM implementation

--- kernel/klimem.c
+++ kernel/klimem.c
@@ -1,4 +1,20 @@
+#include <linux/init.h>       // For module initialization macros
+#include <linux/mm.h>         // For memory management functions and structures
+#include <linux/module.h>     // For module macros and functions
+#include <linux/sched/task.h> // For task_struct and related task functions
+#include <linux/slab.h>       // For kmalloc and kfree
+#include <linux/uaccess.h> // For access_process_vm and user memory access functions
+
 #include "asm/current.h"
+#include "linux/gfp_types.h"
+#include "linux/kern_levels.h"
+#include "linux/mm_types.h"
+#include "linux/pid.h"
+#include "linux/pid_types.h"
+#include "linux/printk.h"
+#include "linux/sched.h"
+#include "linux/sched/mm.h"
+#include "linux/sched/task.h"
 #include <linux/cdev.h> // For character device
 #include <linux/fs.h>   // For file operations structure
 #include <linux/init.h>
@@ -18,23 +34,112 @@ MODULE_DESCRIPTION("Kaylee mem");
 static int major;
 static struct cdev my_cdev;
 
+static int ReadProcessMemoryEx(struct T_RPM args) {
+    pid_t reader_pid = current->pid;
+    struct task_struct *target_task, *reader_task;
+    struct mm_struct *target_mm, *reader_mm;
+    char *buffer;
+    int bytes_read, bytes_written;
+
+    // Locate the target task
+    target_task = get_pid_task(find_get_pid(args.target_pid), PIDTYPE_PID);
+    if (!target_task) {
+        pr_err("<<klimem>> Could not find target task for PID %d\n",
+               args.target_pid);
+        return -ESRCH;
+    }
+
+    target_mm = get_task_mm(target_task);
+    if (!target_mm) {
+        pr_err("<<klimem>> Could not access memory map for target PID %d\n",
+               args.target_pid);
+        put_task_struct(target_task);
+        return -EFAULT;
+    }
+
+    // Locate the reader task
+    reader_task = get_pid_task(find_get_pid(reader_pid), PIDTYPE_PID);
+    if (!reader_task) {
+        pr_err("<<klimem>> Could not find reader task for PID %d\n",
+               reader_pid);
+        mmput(target_mm);
+        put_task_struct(target_task);
+        return -ESRCH;
+    }
+
+    reader_mm = get_task_mm(reader_task);
+    if (!reader_mm) {
+        pr_err("<<klimem>> Could not access reader map for target PID %d\n",
+               reader_pid);
+        mmput(target_mm);
+        put_task_struct(target_task);
+        put_task_struct(reader_task);
+        return -EFAULT;
+    }
+
+    // Allocate kernel buffer for reading data
+    buffer = kmalloc(args.read_size, GFP_KERNEL);
+    if (!buffer) {
+        pr_err("<<klimem>> Failed to allocate buffer\n");
+        mmput(target_mm);
+        mmput(reader_mm);
+        put_task_struct(target_task);
+        put_task_struct(reader_task);
+        return -ENOMEM;
+    }
+
+    // Read the memory from the target process
+    bytes_read = access_process_vm(target_task, args.target_address, buffer,
+                                   args.read_size, FOLL_FORCE);
+    if (bytes_read < 0) {
+        pr_err("<<klimem>> access_process_vm (read) failed\n");
+        kfree(buffer);
+        mmput(target_mm);
+        mmput(reader_mm);
+        put_task_struct(target_task);
+        put_task_struct(reader_task);
+        return -EFAULT;
+    }
+
+    pr_info("<<klimem>> Read %d bytes from process %d at address 0x%lx\n",
+            bytes_read, args.target_pid, args.target_address);
+
+    // Write the data to the reader process
+    bytes_written = access_process_vm(reader_task, args.buffer_address, buffer,
+                                      bytes_read, FOLL_WRITE);
+    if (bytes_written < 0) {
+        pr_err("<<klimem>> access_process_vm (write) failed\n");
+    } else {
+        pr_info("<<klimem>> Wrote %d bytes to process %d at address 0x%lx\n",
+                bytes_written, reader_pid, args.buffer_address);
+    }
+
+    // Cleanup
+    kfree(buffer);
+    mmput(target_mm);
+    mmput(reader_mm);
+    put_task_struct(target_task);
+    put_task_struct(reader_task);
+
+    return 0;
+}
+
 // ioctl handler
 static long ioctl_handler(struct file *file, unsigned int cmd,
                           unsigned long arg) {
 
     struct T_RPM rpm_val;
-    pid_t callerPid;
 
     switch (cmd) {
     case IOCTL_RPM:
         if (copy_from_user(&rpm_val, (int __user *)arg, sizeof(struct T_RPM))) {
             return -EFAULT;
         }
-        callerPid = current->pid;
-        printk(KERN_INFO
-               "RPM Receieved (PID %i, target addr %lx, size %zu, local buff %lx)",
-               rpm_val.target_pid, rpm_val.target_address, rpm_val.read_size,
-               rpm_val.buffer_address);
+        printk(KERN_INFO "RPM Receieved (Caller PID %i) (PID %i, target addr "
+                         "%lx, size %zu, local buff %lx)",
+               current->pid, rpm_val.target_pid, rpm_val.target_address,
+               rpm_val.read_size, rpm_val.buffer_address);
+        ReadProcessMemoryEx(rpm_val);
         break;
 
     default:
--- um_poc/poc.cpp
+++ um_poc/poc.cpp
@@ -1,8 +1,8 @@
 #include "../comm/comm.c"
 
+#include <cstdlib>
 #include <stdio.h>
 #include <unistd.h>
-
 
 int main() {
 
@@ -16,16 +16,35 @@ int main() {
         return -1;
     }
 
-    struct T_RPM toSend;
-    toSend.target_pid = 1234;
-    toSend.buffer_address = 0xDEADBEEF;
-    toSend.read_size = 128;
-    toSend.target_address = 0xDEADC0DE;
-    if (ioctl(fd, IOCTL_RPM, &toSend) < 0) {
-        perror("ioctl rpm set failed");
+    /*struct T_RPM toSend;*/
+    /*toSend.target_pid = 1234;*/
+    /*toSend.buffer_address = 0xDEADBEEF;*/
+    /*toSend.read_size = 128;*/
+    /*toSend.target_address = 0xDEADC0DE;*/
+    /*if (ioctl(fd, IOCTL_RPM, &toSend) < 0) {*/
+    /*    perror("ioctl rpm set failed");*/
+    /*    close(fd);*/
+    /*    return -1;*/
+    /*}*/
+
+    int testVal = 5738;
+    int *readVal = (int *)malloc(sizeof(int));
+
+    struct T_RPM to_send;
+    to_send.target_pid = getpid();
+    to_send.target_address = (unsigned long)&testVal;
+    to_send.read_size = sizeof(int);
+    to_send.buffer_address = (unsigned long)readVal;
+
+    printf("Value in testVal (0x%lx): %i\n", (unsigned long)&testVal, testVal);
+
+    if (ioctl(fd, IOCTL_RPM, &to_send) < 0) {
+        perror("IOCTL RPM failed");
         close(fd);
         return -1;
     }
+
+    printf("Value in readVal (0x%lx): %i\n", (unsigned long)readVal, *readVal);
 
     close(fd);
     return 0;
